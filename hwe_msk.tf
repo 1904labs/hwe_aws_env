f7ff103cfa24b74ec7c67ff6fab2b7c72f402ad0
Tim Sagona <tsagona@1904labs.com>
2024-01-30 12:05:35 -0600

    Temporarily using secret that exists outside of Terraform, since destroying/recreateing it is proving problematic

--- hwe_msk.tf
+++ hwe_msk.tf
@@ -36,10 +36,10 @@ resource "aws_security_group" "sg" {
 }
 
 #Secret for MSK username/password
-resource "aws_secretsmanager_secret" "amazonmsk_hwe_secret" {
-  name = "AmazonMSK_hwe_secret3" #This name MUST start with AmazonMSK_!
-  kms_key_id = aws_kms_key.hwe_kms_key.key_id
-}
+#resource "aws_secretsmanager_secret" "amazonmsk_hwe_secret" {
+#  name = "AmazonMSK_hwe_secret3" #This name MUST start with AmazonMSK_!
+#  kms_key_id = aws_kms_key.hwe_kms_key.key_id
+#}
 
 variable "msk_connection_info" {
   sensitive = true
@@ -50,7 +50,8 @@ variable "msk_connection_info" {
 }
 
 resource "aws_secretsmanager_secret_version" "amazonmsk_hwe_secret_value" {
-  secret_id = aws_secretsmanager_secret.amazonmsk_hwe_secret.id
+  #secret_id = aws_secretsmanager_secret.amazonmsk_hwe_secret.id
+  secret_id = "arn:aws:secretsmanager:us-east-1:153601099083:secret:AmazonMSK_hwe_secret-mqsazU"
   secret_string = jsonencode(var.msk_connection_info)
 }
 
@@ -88,7 +89,8 @@ resource "aws_msk_cluster" "hwe_msk" {
 
 resource "aws_msk_scram_secret_association" "example" {
   cluster_arn     = aws_msk_cluster.hwe_msk.arn
-  secret_arn_list = [aws_secretsmanager_secret.amazonmsk_hwe_secret.arn]
+  #secret_arn_list = [aws_secretsmanager_secret.amazonmsk_hwe_secret.arn]
+  secret_arn_list = ["arn:aws:secretsmanager:us-east-1:153601099083:secret:AmazonMSK_hwe_secret-mqsazU"]
   depends_on = [aws_secretsmanager_secret_version.amazonmsk_hwe_secret_value]
 }
 
@@ -104,12 +106,14 @@ data "aws_iam_policy_document" "secret_access_policy" {
     }
 
     actions   = ["secretsmanager:getSecretValue"]
-    resources = [aws_secretsmanager_secret.amazonmsk_hwe_secret.arn]
+    #resources = [aws_secretsmanager_secret.amazonmsk_hwe_secret.arn]
+    resources = ["arn:aws:secretsmanager:us-east-1:153601099083:secret:AmazonMSK_hwe_secret-mqsazU"]
   }
 }
 
 resource "aws_secretsmanager_secret_policy" "msk_secret_policy" {
-  secret_arn = aws_secretsmanager_secret.amazonmsk_hwe_secret.arn
+  #secret_arn = aws_secretsmanager_secret.amazonmsk_hwe_secret.arn
+  secret_arn = "arn:aws:secretsmanager:us-east-1:153601099083:secret:AmazonMSK_hwe_secret-mqsazU"
   policy     = data.aws_iam_policy_document.secret_access_policy.json
 }
 
